01e8dcbc8590d2451ce036852476fe6303a432fd
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-02 12:13:43 +0100

    Create form for new article

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'welcome#index' #to jest metoda, ktora odwoluje sie do metody index, jest przerabiane na odpowiednia klase, wszystko jest w dokumentacji railsow. To jest odwolanie do controllera
 
   get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  post 'articles', to: 'articles#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
