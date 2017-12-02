fa77ec8ba2b9aa5062e77aeebb6742b15bee5461
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-02 11:17:50 +0100

    Add Articles Controlles with index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  root 'welcome#index' #to jest metoda, ktora odwoluje sie do metody index, jest przerabiane na odpowiednia klase, wszystko jest w dokumentacji railsow
+  root 'welcome#index' #to jest metoda, ktora odwoluje sie do metody index, jest przerabiane na odpowiednia klase, wszystko jest w dokumentacji railsow. To jest odwolanie do controllera
+
+  get 'articles', to: 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
