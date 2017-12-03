477815004b98f28a7a428bf9ecbf96d02d2ffc5a
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-03 10:47:11 +0100

    Change routes to simple way

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
 
   root 'welcome#index' #to jest metoda, ktora odwoluje sie do metody index, jest przerabiane na odpowiednia klase, wszystko jest w dokumentacji railsow. To jest odwolanie do controllera
 
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: 'new_article'
-  post 'articles', to: 'articles#create'
-  get 'articles/:id', to: 'articles#show', as: 'article'
-  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
+  #get 'articles', to: 'articles#index'
+  #get 'articles/new', to: 'articles#new', as: 'new_article'
+  #post 'articles', to: 'articles#create'
+  #get 'articles/:id', to: 'articles#show', as: 'article'
+  #get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  #patch 'articles/:id', to: 'articles#update'
+  #delete 'articles/:id', to: 'articles#destroy'
+
+  resources :articles # zamiast tego co powyzej mozna zapisac tak i dziala:))))
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
