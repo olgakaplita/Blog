a45f62a9effed88d8afbadd9ae7c9a83bab3e107
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-16 12:55:43 +0100

    Add exercise 1 (validation) and 2 (constraints) and 3 (styles)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :commenter, presence: true
-  validates :body, presence: true, length: {in: 6..500} 
+  validates :commenter, presence: true, email: true
+  validates :body, presence: true, length: {in: 6..500}
 end
---
+++ db/migrate/20171216103406_add_constraint_to_comment.rb
@@ -0,0 +1,6 @@
+class AddConstraintToComment < ActiveRecord::Migration[5.1]
+  def change
+    change_column_null :comments, :commenter, false
+    change_column_null :comments, :body, false
+  end
+end
