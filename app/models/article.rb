4b4bde9275ea82b9d47bd2d6fe1b398c142b50fd
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-16 15:02:03 +0100

    Add tag field do article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+
+  def tags=(value)
+    value = sanitize_tags(value) if value.is_a?(String)
+    super(value)
+  end
+
+  private
+
+   def sanitize_tags(value)
+     values.downcase.split.uniq
+   end
+
 end
