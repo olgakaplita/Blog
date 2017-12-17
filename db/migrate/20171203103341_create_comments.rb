1aed2b4b4bc5efd08ea0200fa791a380e423c73d
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-17 09:31:10 +0100

    Add author_id column

--- db/migrate/20171203103341_create_comments.rb
+++ db/migrate/20171203103341_create_comments.rb
@@ -4,8 +4,10 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.string :commenter
       t.text :body
       t.references :article, foreign_key: true
+      # i.integer :article_id, index: true - inny zapis linijki powyzej
 
       t.timestamps
     end
+    add_foreign_key :comments, :articles
   end
 end
