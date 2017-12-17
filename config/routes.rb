0a0b4a288302700403a5832ca029a42e73e9b796
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2017-12-17 14:45:05 +0100

    Add like and dislike option

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,12 @@ Rails.application.routes.draw do
   #delete 'articles/:id', to: 'articles#destroy'
 
   resources :articles do # zamiast tego co powyzej mozna zapisac tak i dziala:))))
-    resources :comments
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
+
+  # POST articles/:article_id/likes
+  # POST likes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
