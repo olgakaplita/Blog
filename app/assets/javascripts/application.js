41ebe3b0eb85a239798d757658be38f97e9cbfd9
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2018-01-14 12:16:30 +0100

    Add JS simple calculator on home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,24 @@
 
 //= require jquery
 //= require bootstrap-sprockets
+
+window.addEventListener("load", function() {
+//  document.getElementById("q").value = "form javascript";
+
+//  document.getElementById("q").addEventListener('keypress', function() {
+//     console.log(this.value)
+//   })
+//   document.getElementById("q").addEventListener('mouseover', function () {
+//     console.log('myszka nad formularzem')
+//   })
+document.getElementById("add").addEventListener("click", function () {
+    var wynik;
+    var field1Value = parseInt(document.getElementById('field1').value);
+    var field2Value = parseInt(document.getElementById('field2').value);
+
+
+    wynik = field1Value + field2Value;
+    document.getElementById("wynik").innerHTML = wynik
+  })
+
+ });
