e1a66bc45b5476e0cbf643fe832f7c45aea447cd
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2018-02-11 11:08:55 +0100

    Add scope

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,9 @@ class Like <ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true #przedtem like nalezal do artykulu, teraz do concerna
 
+  scope :last_test_user_like, -> { last_user_like("test@example.com") }
+
+  scope :last_user_like, ->(email) { joins(:user).where("users.email" => email).limit(1) }
+
   validates :user, uniqueness: { scope: :likeable, message: "already liked" }
 end
