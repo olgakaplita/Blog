34c5518cae44013be4405d877cbc5c6ab3b932c2
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2018-02-11 10:25:28 +0100

    Change article/likeable in controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
-    like = Like.new(article_id: params[:article_id], user: current_user)
+    like = Like.new(likeable_id: params[:article_id], likeable_type: "Artticle", user: current_user)
     like.save
 
     redirect_to article_path(like.article)
